7de443b42637877c0f047db3911424810639cd3f
HeyHorsey <dedmene@gmail.com>
2020-05-12 12:42:11 +0300

    0.8

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,8 +4,8 @@ t=`tty`
 tt=`echo $t | sed -En "s/\/dev\///p"`
 res=`who | grep $tt | sed -En "s/^.*(\(.*\)).*$/\1/p"`
 #curl -s -X POST https://api.telegram.org/botTOKEN/sendMessage -d chat_id=CHATID -d text="$res PROJ VERSION stopping" --proxy1.0 cache.solvo.ru:3128 &> /dev/null
-#/etc/init.d/dms-AET-gf stop
-/etc/init.d/dms-AET-pf stop
-#/etc/init.d/dms-AET-xml stop
-/etc/init.d/dms-AET stop
+#/etc/init.d/dms-PROJ-gf stop
+/etc/init.d/dms-PROJ-pf stop
+#/etc/init.d/dms-PROJ-xml stop
+/etc/init.d/dms-PROJ stop
 #curl -s -X POST https://api.telegram.org/botTOKEN/sendMessage -d chat_id=CHATID -d text="$res PROJ VERSION stopped" --proxy1.0 cache.solvo.ru:3128 &> /dev/null
