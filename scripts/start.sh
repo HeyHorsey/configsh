9a08ae28fe7f1e89d31c8420fc9c490b46bea30b
HeyHorsey <dedmene@gmail.com>
2020-05-19 22:09:37 +0300

    1.2

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ t=`tty`
 tt=`echo $t | sed -En "s/\/dev\///p"`
 res=`who | grep $tt | sed -En "s/^.*(\(.*\)).*$/\1/p"`
 #curl -s -X POST https://api.telegram.org/botTOKEN/sendMessage -d chat_id=CHATID -d text="$res PROJ VERSION starting" PROXY &> /dev/null
+pkill -f opt/solvo -u PROJ
 /etc/init.d/dms-PROJ start
 #/etc/init.d/dms-PROJ-xml start
 #/etc/init.d/dms-PROJ-gf start
