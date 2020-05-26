561bdf666e065d6a00276746ef19cc5f01a93b6d
HeyHorsey <dedmene@gmail.com>
2020-05-26 16:37:38 +0300

    2.3

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-UVPVER=$(rpm -qa --qf '%{VERSION}' uvp-win32)
-WTMVER=$(rpm -qa --qf '%{VERSION}' wtm3-win32)
-SMCVER=$(rpm -qa --qf '%{VERSION}' wmsmc-win32)
-CVVER=$(rpm -qa --qf '%{VERSION}' ctms_vessel-win32)
-CTVER=$(rpm -qa --qf '%{VERSION}' ctms_train-win32)
-KIOSKVER=$(rpm -qa --qf '%{VERSION}' solvo-KioskPROJ)
-XRDTVER=$(rpm -qa --qf '%{VERSION}' solvo-xrdt-PROJ)
+UVPVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' uvp-win32)
+WTMVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' wtm3-win32)
+SMCVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' wmsmc-win32)
+CVVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' ctms_vessel-win32)
+CTVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' ctms_train-win32)
+KIOSKVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' solvo-KioskPROJ)
+XRDTVER=$(rpm -qa --qf '%{VERSION}-%{RELEASE}' solvo-xrdt-PROJ)
 
 echo "UVP:        "$UVPVER "\nWTM:         "$WTMVER "\nWMSMC:       "$SMCVER "\nCTMS VESSEL: "$CVVER "\nCTMS TRAIN:  "$CTVER "\nKIOSK:       "$KIOSKVER "\nXRDT:        "$XRDTVER
