7ddfc677634a1dde3901f26010553937d2380ce0
HeyHorsey <dedmene@gmail.com>
2020-09-25 13:30:28 +0300

    2.14 Post install automation improved

--- scripts/update.sh
+++ scripts/update.sh
@@ -26,13 +26,15 @@ sudo -i -u PROJ sh -c '#curl -F document=@mdd/patch.err.log https://api.telegram
 
 # Post install
 until [ "$pinst" == "$(sudo -i -u PROJ sh -c 'md5sum post_install_sql.err.log')" ]
+echo "Running post_install"
 do
   sudo -i -u PROJ sh -c 'post_install_sql &> ~/post_install_sql.log'
   sudo -i -u PROJ sh -c 'grep -5 ORA ~/post_install_sql.log > post_install_sql.err.log'
-  sudo -i -u PROJ sh -c '#curl -s -X POST https://api.telegram.org/botTOKEN/sendMessage -d chat_id=CHATID -d text="Please review post_install error log." &> /dev/null'
-  sudo -i -u PROJ sh -c '#curl -F document=@post_install_sql.err.log https://api.telegram.org/botTOKEN/sendDocument?chat_id=CHATID &> /dev/null'
   export pinst="$(sudo -i -u PROJ sh -c 'md5sum post_install_sql.err.log')"
 done
+sudo -i -u PROJ sh -c '#curl -s -X POST https://api.telegram.org/botTOKEN/sendMessage -d chat_id=CHATID -d text="Please review post_install error log." &> /dev/null'
+sudo -i -u PROJ sh -c '#curl -F document=@post_install_sql.err.log https://api.telegram.org/botTOKEN/sendDocument?chat_id=CHATID &> /dev/null'
+
 #sudo -i -u PROJ sh -c 'sqlplus -S $DB_USER/$DB_PASSWD@$DB_NAME @ /sql_ee/catalog.sql'
 
 # Print versions for delivery note
